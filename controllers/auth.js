16eb49211c5a4217e1ed622386b242c883ce27fb
Andres Hiran <andrescomte@gmail.com>
2022-05-18 14:30:02 -0400

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 
 const bcryptjs = require('bcryptjs');
 const {response} = require('express');
+const { json } = require('express/lib/response');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/usuario');
 
 
@@ -55,6 +57,57 @@ const login = async(req,res = response) =>{
     }
 }
 
+const googleSignIn = async(req,res = response) =>{
+
+    const {id_token} = req.body;
+
+    try {
+
+        const {nombre,img,correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            //tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol:'USER_ROLE',
+                google:true,
+
+            };
+            usuario = new Usuario(data)
+            await usuario.save();
+        }
+
+        //si el usuario en db 
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg:"Hable con el administrador, usuario bloquiado"
+            })
+        }
+
+        //generar json webtoken
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg:"El token no se pudo verificar"
+        })
+        
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
